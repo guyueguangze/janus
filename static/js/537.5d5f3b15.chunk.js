05538bf78d1816c3df2e86cb71d06728cb0add14
huguangze <2581504687@qq.com>
2024-01-30 13:36:13 +0800

    Updates

---
+++ static/js/537.5d5f3b15.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[537],{4537:function(s,e,i){i.r(e),i.d(e,{default:function(){return n}});i(2791);var c={root:"Resources_root__KsypA"},a=i(184);function n(){return(0,a.jsx)("div",{className:c.root,children:(0,a.jsxs)("div",{className:"resource_container",children:[(0,a.jsx)("div",{className:"publications",children:"publications"}),(0,a.jsx)("div",{className:"slides",children:"slides"}),(0,a.jsx)("div",{className:"videos",children:"videos"})]})})}}}]);
+//# sourceMappingURL=537.5d5f3b15.chunk.js.map
--- static/js/585.b8a4a1b4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[585],{1585:function(e,n,u){u.r(n),u.d(n,{default:function(){return c}});u(2791);var r=u(184);function c(){return(0,r.jsx)("div",{children:"Resources"})}}}]);
-//# sourceMappingURL=585.b8a4a1b4.chunk.js.map
